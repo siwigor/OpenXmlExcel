164a285e3c696113167d9c78157b8e0ff844367f
siwigor <siwigor@gmail.com>
2015-10-08 16:18:56 +0300

    Update OpenXmlExcel.cs

--- OpenXmlExcel.cs
+++ OpenXmlExcel.cs
@@ -8,25 +8,68 @@ using DocumentFormat.OpenXml;
 using System.Collections;
 using DocumentFormat.OpenXml.Spreadsheet;
 using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace OpenXmlExcel
 {
     public class ExcelDocument : IDisposable
     {
         public SpreadsheetDocument document;
+        public Stylesheet stylesheet;
         public ExcelSheetCollection Sheets;
+        public static uint DateTypeIndex;
 
         private bool disposed = false;
 
         public ExcelDocument(string filename)
         {
             if (File.Exists(filename))
+            {
                 document = SpreadsheetDocument.Open(filename, true);
+                if (document.WorkbookPart.GetPartsOfType<WorkbookStylesPart>().Count() == 0)
+                {
+                    WorkbookStylesPart stylespart = document.WorkbookPart.AddNewPart<WorkbookStylesPart>();
+                    stylespart.Stylesheet = GetStylesheet();
+                    DateTypeIndex = 1;
+                    stylesheet = stylespart.Stylesheet;
+                }
+                else
+                {
+                    WorkbookStylesPart stylespart = document.WorkbookPart.GetPartsOfType<WorkbookStylesPart>().First();
+                    IEnumerable<CellFormat> cellformats = stylespart.Stylesheet.CellFormats.Elements<CellFormat>();
+                    if (cellformats.Where(f => f.NumberFormatId == 14).Count() > 0)
+                    {
+                        uint dateformatindex = 0;
+                        foreach (CellFormat format in cellformats)
+                            if (format.NumberFormatId == 14) break; else dateformatindex++;
+                        DateTypeIndex = dateformatindex;
+                    }
+                    else
+                    {
+                        stylespart.Stylesheet.CellFormats.Append(new CellFormat()
+                        {
+                            BorderId = 0,
+                            FillId = 0,
+                            FontId = 0,
+                            NumberFormatId = 14,
+                            FormatId = 0,
+                            ApplyNumberFormat = true
+                        });
+                        stylespart.Stylesheet.CellFormats.Count = (uint)stylespart.Stylesheet.CellFormats.ChildElements.Count;
+                        DateTypeIndex = stylespart.Stylesheet.CellFormats.Count - 1;
+                    }
+                    stylesheet = stylespart.Stylesheet;
+                }
+            }
             else
             {
                 document = SpreadsheetDocument.Create(filename, SpreadsheetDocumentType.Workbook);
                 WorkbookPart workbookpart = document.AddWorkbookPart();
                 workbookpart.Workbook = new Workbook();
+                WorkbookStylesPart stylespart = workbookpart.AddNewPart<WorkbookStylesPart>();
+                stylespart.Stylesheet = GetStylesheet();
+                DateTypeIndex = 1;
+                stylesheet = stylespart.Stylesheet;
             }
             Sheets = new ExcelSheetCollection(this);
         }
@@ -53,6 +96,83 @@ namespace OpenXmlExcel
             shareStringPart.SharedStringTable.AppendChild(new SharedStringItem(new DocumentFormat.OpenXml.Spreadsheet.Text(text)));
 
             return i;
+        }
+
+        private static Stylesheet GetStylesheet()
+        {
+            Stylesheet stylesheet = new Stylesheet();
+            Fonts fonts = new Fonts();
+            fonts.Append(new Font()
+            {
+                FontName = new FontName() { Val = "Calibri" },
+                FontSize = new FontSize() { Val = 11 },
+                FontFamilyNumbering = new FontFamilyNumbering() { Val = 2 },
+            });
+            fonts.Count = (uint)fonts.ChildElements.Count;
+            
+            Fills fills = new Fills();
+            fills.Append(new Fill() { PatternFill = new PatternFill() { PatternType = PatternValues.None } });
+            fills.Append(new Fill() { PatternFill = new PatternFill() { PatternType = PatternValues.Gray125 } });
+            fills.Count = (uint)fills.ChildElements.Count;
+
+            Borders borders = new Borders();
+            borders.Append(new Border()
+            {
+                LeftBorder = new LeftBorder(),
+                RightBorder = new RightBorder(),
+                TopBorder = new TopBorder(),
+                BottomBorder = new BottomBorder(),
+                DiagonalBorder = new DiagonalBorder()
+            });
+            borders.Count = (uint)borders.ChildElements.Count;
+
+            CellStyleFormats cellstyleformats = new CellStyleFormats();
+            cellstyleformats.Append(new CellFormat()
+            {
+                NumberFormatId = 0,
+                FontId = 0,
+                FillId = 0,
+                BorderId = 0
+            });
+            cellstyleformats.Count = (uint)cellstyleformats.ChildElements.Count;
+
+            CellFormats cellformats = new CellFormats();
+            cellformats.Append(new CellFormat()
+            {
+                BorderId = 0,
+                FillId = 0,
+                FontId = 0,
+                NumberFormatId = 0,
+                FormatId = 0
+            });
+            cellformats.Append(new CellFormat()
+            {
+                BorderId = 0,
+                FillId = 0,
+                FontId = 0,
+                NumberFormatId = 14,
+                FormatId = 0,
+                ApplyNumberFormat = true
+            });
+            cellformats.Count = (uint)cellformats.ChildElements.Count;
+
+            CellStyles cellstyles = new CellStyles();
+            cellstyles.Append(new CellStyle()
+            {
+                Name = "Normal",
+                FormatId = 0,
+                BuiltinId = 0
+            });
+            cellstyles.Count = (uint)cellstyles.ChildElements.Count;
+
+            stylesheet.Append(fonts);
+            stylesheet.Append(fills);
+            stylesheet.Append(borders);
+            stylesheet.Append(cellstyleformats);
+            stylesheet.Append(cellformats);
+            stylesheet.Append(cellstyles);
+
+            return stylesheet;
         }
 
         public void Close()
@@ -183,6 +303,17 @@ namespace OpenXmlExcel
                 sheetName = getSheetName(name, sheets, number + 1);
             return sheetName;
         }
+
+        public void RemoveCell(uint rowIndex, uint colIndex)
+        {
+            if (_rows.Contains(rowIndex))
+                _rows[rowIndex].Cells.Remove(colIndex);
+        }
+
+        public void RemoveCell(string address)
+        {
+            RemoveCell(ExcelCell.GetRowIndex(address), ExcelCell.GetColumnIndex(ExcelCell.GetColumnName(address)));
+        }
     }
 
     public class ExcelRowCollection
@@ -200,6 +331,11 @@ namespace OpenXmlExcel
         }
 
         public int Length { get { return this.List.Count; } }
+
+        public bool Contains(uint rowIndex)
+        {
+            return this.List.ContainsKey(rowIndex);
+        }
 
         public ExcelRowCollection(ExcelSheet sheet)
         {
@@ -252,6 +388,16 @@ namespace OpenXmlExcel
 
         public int Length { get { return this.List.Count; } }
 
+        public bool Contains(uint colIndex)
+        {
+            return this.List.ContainsKey(colIndex);
+        }
+
+        public bool Contains(string colName)
+        {
+            return this.List.ContainsKey(ExcelCell.GetColumnIndex(colName));
+        }
+
         public ExcelCell this[string name]
         {
             get 
@@ -288,6 +434,20 @@ namespace OpenXmlExcel
             }
         }
 
+        public void Remove(uint index)
+        {
+            if (this.List.ContainsKey(index))
+            {
+                this.List[index].cell.Remove();
+                this.List.Remove(index);
+            }
+        }
+
+        public void Remove(string colName)
+        {
+            Remove(ExcelCell.GetColumnIndex(colName));
+        }
+
         public ExcelCell GetCell(uint index)
         {
             if (index < 1) throw new ArgumentException("Index column cannot be less than 1!");
@@ -316,7 +476,7 @@ namespace OpenXmlExcel
         public ExcelRow row;
         public Cell cell;
 
-        public string Value
+        public object Value
         {
             get { return getCellValue(); }
             set { setCellValue(value); }
@@ -328,9 +488,10 @@ namespace OpenXmlExcel
             this.cell = cell;
         }
 
-        private string getCellValue()
+        private object getCellValue()
         {
-            string value = cell.InnerText;
+            string cellText = cell.InnerText;
+            object value = cellText;
             if (cell.DataType != null)
             {
                 switch (cell.DataType.Value)
@@ -338,30 +499,112 @@ namespace OpenXmlExcel
                     case CellValues.SharedString:
                         var stringTable = row.sheet.document.document.WorkbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
                         if (stringTable != null)
-                            value = stringTable.SharedStringTable.ElementAt(int.Parse(value)).InnerText;
+                            value = stringTable.SharedStringTable.ElementAt(int.Parse(cellText)).InnerText;
                         break;
                     case CellValues.Boolean:
-                        switch (value)
-                        {
-                            case "0":
-                                value = "FALSE";
-                                break;
-                            default:
-                                value = "TRUE";
-                                break;
-                        }
+                        if(cellText == "0") value = false; else value = true;
+                        break;
+                    case CellValues.Number:
+                        value = Convert.ToDecimal(cellText, CultureInfo.InvariantCulture);
+                        break;
+                    case CellValues.Date:
+                        value = DateTime.FromOADate(Convert.ToDouble(cellText, CultureInfo.InvariantCulture));
+                        break;
+                    default:
+                        value = cellText;
                         break;
                 }
             }
             return value;
         }
 
-        private void setCellValue(string value)
+        private void setCellValue(object value)
         {
-            int index = row.sheet.document.InsertSharedStringItem(value);
-            cell.CellValue = new CellValue(index.ToString());
-            cell.DataType = new EnumValue<CellValues>(CellValues.SharedString);
+            string text = Convert.ToString(value, CultureInfo.InvariantCulture);
+            if (String.IsNullOrEmpty(text)) return;
+            if(value is string)
+            {
+                int index = row.sheet.document.InsertSharedStringItem(text);
+                cell.CellValue = new CellValue(index.ToString());
+                cell.DataType = new EnumValue<CellValues>(CellValues.SharedString);
+            }
+            else if(ExcelCell.IsNumericType(value.GetType()))
+            {
+                cell.CellValue = new CellValue(text);
+                cell.DataType = new EnumValue<CellValues>(CellValues.Number);
+            }
+            else
+            {
+                cell.CellValue = new CellValue(text);
+                cell.DataType = new EnumValue<CellValues>(CellValues.String);
+            }
+            
         }
+
+        public string GetString()
+        {
+            string value = cell.InnerText;
+            if (cell.DataType != null)
+            {
+                if (cell.DataType.Value == CellValues.SharedString)
+                {
+                    var stringTable = row.sheet.document.document.WorkbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+                    if (stringTable != null)
+                        value = stringTable.SharedStringTable.ElementAt(int.Parse(value)).InnerText;
+                }
+            }
+            return value;
+        }
+
+        public bool GetBoolean()
+        {
+            return (cell.InnerText != "0");
+        }
+
+        public decimal GetNumber()
+        {
+            return Convert.ToDecimal(cell.InnerText, CultureInfo.InvariantCulture);
+        }
+
+        public DateTime GetDate()
+        {
+            return DateTime.FromOADate(Convert.ToDouble(cell.InnerText, CultureInfo.InvariantCulture));
+        }
+
+        public void SetValue(object value, ExcelCellType type)
+        {
+            string text = Convert.ToString(value, CultureInfo.InvariantCulture);
+            if (String.IsNullOrEmpty(text)) return;
+            switch (type)
+            {
+                case ExcelCellType.String:
+                    cell.CellValue = new CellValue(text);
+                    cell.DataType = new EnumValue<CellValues>(CellValues.String);
+                    break;
+                case ExcelCellType.SharedString:
+                    int index = row.sheet.document.InsertSharedStringItem(text);
+                    cell.CellValue = new CellValue(index.ToString());
+                    cell.DataType = new EnumValue<CellValues>(CellValues.SharedString);
+                    break;
+                case ExcelCellType.Number:
+                    cell.CellValue = new CellValue(text);
+                    cell.DataType = new EnumValue<CellValues>(CellValues.Number);
+                    break;
+                case ExcelCellType.Date:
+                    text = Convert.ToDateTime(value).ToOADate().ToString(CultureInfo.InvariantCulture);
+                    cell.CellValue = new CellValue(text);
+                    cell.DataType = CellValues.Number;
+                    cell.StyleIndex = ExcelDocument.DateTypeIndex;
+                    break;
+                case ExcelCellType.Boolean:
+                    text = Convert.ToInt32(value, CultureInfo.InvariantCulture).ToString();
+                    cell.CellValue = new CellValue(text);
+                    cell.DataType = new EnumValue<CellValues>(CellValues.Boolean);
+                    break;
+            }
+        }
+
+        public enum ExcelCellType { String, SharedString, Number, Date, Boolean };
 
         public static string ConvertToLetter(uint iCol)
         {
@@ -394,6 +637,35 @@ namespace OpenXmlExcel
             foreach (char leter in leters)
                 result = (((uint)leter) - 64) + (result * 26);
             return result;
+        }
+
+        private static bool IsNumericType(Type type)
+        {
+            if (type == null)
+                return false;
+
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Byte:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.SByte:
+                case TypeCode.Single:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                    return true;
+                case TypeCode.Object:
+                    if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+                    {
+                        return IsNumericType(Nullable.GetUnderlyingType(type));
+                    }
+                    return false;
+            }
+            return false;
         }
     }
 }
